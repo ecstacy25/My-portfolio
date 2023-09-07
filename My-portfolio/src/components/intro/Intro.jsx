b38667441980bad46f352d895170687c02d0d50c
ecstacy25 <ibiamjnr9@gmail.com>
2023-09-07 11:32:55 +0100

    Done

--- My-portfolio/src/components/intro/Intro.jsx
+++ My-portfolio/src/components/intro/Intro.jsx
@@ -24,7 +24,7 @@ const Intro = () => {
             <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>3+ year</small>
+              <small>3+ years</small>
             </article>
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
